14a9d24e58b157830b4c1f1da7cb31b467d4eb9a
Paridhi Gupta <paridhigupta891@gmail.com>
2025-11-12 09:47:00 +0530

    Update pass.js

--- HospitalManagement/build/web/myjs/pass.js
+++ HospitalManagement/build/web/myjs/pass.js
@@ -1,7 +1,3 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
- */
 
 // Password strength checker
 const passwordInput = document.getElementById('password');
